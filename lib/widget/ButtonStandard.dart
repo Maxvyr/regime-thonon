d330cc8f9f8353bd015d05a0c28fe50afa033190
VIDALINC Maxime <maxvy_@naver.com>
2020-02-18 13:36:49 +0100

    custom button

--- lib/widget/ButtonStandard.dart
+++ lib/widget/ButtonStandard.dart
@@ -1,12 +1,12 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:regime_thonon/controller/Colors.dart';
 
 class ButtonStandard extends StatelessWidget {
   final String route;
   final String text;
-  const ButtonStandard(this.route, this.text,{
+  const ButtonStandard(
+    this.route,
+    this.text, {
     Key key,
   }) : super(key: key);
 
@@ -14,13 +14,20 @@ class ButtonStandard extends StatelessWidget {
   Widget build(BuildContext context) {
     return RaisedButton(
       onPressed: () {
-      Navigator.pushNamed(context, route);
-    },
-    color: greenMain,
-    textColor: white,
-    elevation: 15.0,
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-    child: Text(text),
+        Navigator.pushNamed(context, route);
+      },
+      color: greenMain,
+      textColor: white,
+      elevation: 15.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Container(
+        width: MediaQuery.of(context).size.width*0.5,
+        child: Center(
+          child: Text(
+            text,
+          ),
+        ),
+      ),
     );
   }
 }
